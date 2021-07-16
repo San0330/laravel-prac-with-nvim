b729f0f50b95591d3b40714f27f2a5066d8b5b6b
Nepali-San <sanneupane1024@gmail.com>
2021-07-16 12:11:04 +0545

    added Post migration & model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,58 +1,11 @@
-<?php 
+<?php
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post{
-
-    public string $title;
-    public string $excerpt;
-    public string $date;
-    public string $body;
-    public string $slug;
-
-    public function __construct(string $title,string $excerpt, string $date, string $body, string $slug){
-        $this->title = $title;
-        $this->date = $date;
-        $this->excerpt = $excerpt;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all(){
-        return cache()->rememberForever('post.all', fn () => 
-            collect(File::files(resource_path("posts")))
-                ->map(function($file){
-                    return YamlFrontMatter::parseFile($file);
-                })
-                ->map(function($document){
-                    return new Post(
-                        $document->title,
-                        $document->excerpt,
-                        $document->date,
-                        $document->body(),
-                        $document->slug
-                    );
-                })
-                ->sortByDesc('date')
-        );
-    }
-
-    public static function find($slug){
-        return static::all()->firstWhere('slug',$slug);
-    }
-
-    public static function findOrFail($slug){
-        $post = static::find($slug); 
-
-        if(!$post){
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }    
-
+class Post extends Model
+{
+    use HasFactory;
 }
