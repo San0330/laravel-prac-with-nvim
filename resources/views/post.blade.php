86e065a8fa5d48119d5aff65835e2ef110140a35
Nepali-San <sanneupane1024@gmail.com>
2021-07-19 01:31:06 +0545

    update url links on views

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,8 @@
 <x-layout>
     <article>
        <h1> {{ $post->title }} </h1>
-       <i>By <a href="#"> {{ $post->user->name }} </a> in {{ $post->category->name }}</i>
+       <i>By <a href="#"> {{ $post->user->name }} </a> in 
+       <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</i></a>
        <p> {{ $post->body }} </p>
     </article>
     <a href="/">Go Back</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,11 +2,11 @@
     @foreach ($posts as $post)
     <article>
         <h1>
-           <a href="posts/{{ $post->slug }}">
+           <a href="/posts/{{ $post->slug }}">
                 {{ $post->title}} 
            </a>
         </h1>
-       <a href="categories/{{ $post->category->slug }}">{{ $post->category->name }}</i></a>
+       <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</i></a>
        <p>{{ $post->excerpt }}</p> 
     </article>
     @endforeach
