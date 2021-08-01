852aba7b7b4070fb3b59aa91d1a5e61c0d38576e
Nepali-San <sanneupane1024@gmail.com>
2021-08-01 22:37:34 +0545

    updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,16 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Post::truncate();
 
+        $user3 = User::factory()->create([
+            'name' => 'Santosh Neupane'
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user3->id
+        ]);
+
         $user = User::factory()->create();
+        $user2 = User::factory()->create();
         
         $personal = Category::create([
             'name' => 'Personal',
@@ -62,6 +71,33 @@ class DatabaseSeeder extends Seeder
             'body' => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
             'category_id' => $work->id,
             'user_id' => $user->id
+        ]);
+
+        Post::create([
+            'title' => 'Best Family',
+            'slug' => 'my-best-family',
+            'excerpt' => 'Lorem ipsum doler sit amet.',
+            'body' => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+            'category_id' => $family->id,
+            'user_id' => $user2->id
+        ]);
+
+        Post::create([
+            'title' => 'A day in the life of developer',
+            'slug' => 'a-day-in-life-developer',
+            'excerpt' => 'Lorem ipsum doler sit amet.',
+            'body' => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+            'category_id' => $work->id,
+            'user_id' => $user2->id
+        ]);
+        
+        Post::create([
+            'title' => 'How to sleep well',
+            'slug' => 'sleep-well',
+            'excerpt' => 'Lorem ipsum doler sit amet.',
+            'body' => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+            'category_id' => $personal->id,
+            'user_id' => $user2->id
         ]);
 
     }
