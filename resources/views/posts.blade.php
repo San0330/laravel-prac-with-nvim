6346986ee782f6a72dc51d3d6c7adcb5f82bbc34
san0330 <sanneupane1024@gmail.com>
2021-10-24 10:15:36 +0545

    populating posts UI with database values

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,17 +2,11 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center">No Posts Yet ! Please check back later.</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
     </main>
 </x-layout>
