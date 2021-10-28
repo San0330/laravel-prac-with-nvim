f1984a6eb1df690b033972da5dd3615bc49d5dcb
san0330 <sanneupane1024@gmail.com>
2021-10-28 10:37:32 +0545

    update category using alpinejs

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,9 +1,12 @@
 <!doctype html>
 
-<title>Laravel From Scratch Blog</title>
+<title>Laravel Blog</title>
+
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
--- routes/web.php
+++ routes/web.php
@@ -36,12 +36,14 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all(),
     ]);
 });
 
 Route::get('author/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,        
     ]);
 });
