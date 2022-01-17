701842c03533ebb1affe1ddacecdb68370c81b80
san0330 <sanneupane1024@gmail.com>
2022-01-17 12:05:28 +0545

    added comment form + minor updates

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Post;
 use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
@@ -24,7 +25,7 @@ class CommentFactory extends Factory
     {
         return [
             'post_id' => Post::factory(),
-            'user_id' => Post::factory(),
+            'user_id' => User::factory(),
             'body' => $this->faker->paragraph(),
         ];
     }
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,16 +1,17 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" height="60px" width="60px" />
+        <img class="rounded-md" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" height="60px" width="60px" />
     </div>
     <div>
         <header class="mb-4 rounded-xl">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->name }}</h3>
             <p class="text-xs">
                 Posted
                 <time>8 minutes ago</time>
             </p>
         </header>
-        <p>Voluptate quis nisi suscipit qui. Mollitia est hic impedit ipsum voluptatum commodi. Sit quia molestiae
-            quisquam illum ad suscipit. Aut non dolores quia illo eos excepturi.</p>
+        <p>{{ $comment->body }}</p>
     </div>
 </article>
