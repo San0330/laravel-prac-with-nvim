77fa772b52a2f97cb7ff169b379f7709ac0d6362
san0330 <sanneupane1024@gmail.com>
2022-01-26 13:50:41 +0545

    extracted panel component

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,17 +1,19 @@
 @props(['comment'])
-
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img class="rounded-md" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" height="60px" width="60px" />
-    </div>
-    <div>
-        <header class="mb-4 rounded-xl">
-            <h3 class="font-bold">{{ $comment->author->name }}</h3>
-            <p class="text-xs">
-                Posted
-                <time>8 minutes ago</time>
-            </p>
-        </header>
-        <p>{{ $comment->body }}</p>
-    </div>
-</article>
+<x-panel class="bg-gray-100">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img class="rounded-md" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" height="60px"
+                width="60px" />
+        </div>
+        <div>
+            <header class="mb-4 rounded-xl">
+                <h3 class="font-bold">{{ $comment->author->name }}</h3>
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at->diffForHumans() }}</time>
+                </p>
+            </header>
+            <p>{{ $comment->body }}</p>
+        </div>
+    </article>
+</x-panel>
