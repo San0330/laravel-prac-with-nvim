e696cad82770dac79925b48c05eb3ac73ab301f1
san0330 <sanneupane1024@gmail.com>
2022-02-08 11:49:46 +0545

    added newsletter interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Services\Newsletter;
+use MailchimpMarketing\ApiClient;
+use App\Services\MailchimpNewsletter;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,7 +17,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient())->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us14'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
---
+++ app/Services/ConvertKitNewsletter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+
+class ConvertKitNewsletter implements Newsletter
+{
+    public function subscribe($email, string $list = null)
+    {
+    }
+}
